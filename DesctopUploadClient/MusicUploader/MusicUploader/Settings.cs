b9866cdc7ef256f406a2944546512daecf458cdc
Dmitry Panchenko <panchenkodp@gmail.com>
2016-09-05 11:49:21 +0300

    WinForms клиент для загрузки музыки на сервер

---
+++ DesctopUploadClient/MusicUploader/MusicUploader/Settings.cs
@@ -0,0 +1,11 @@
+﻿namespace OwnRadio.DesktopPlayer
+{
+    // Настройки программы
+    class Settings
+    {
+        // Путь к файлу на сервере
+        public string serverAddress { get; set; }
+        // Имя файла
+        public string userId { get; set; }
+    }
+}
