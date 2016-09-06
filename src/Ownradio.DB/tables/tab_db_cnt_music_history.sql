3ff062e60f1d362083a0fb8868760c5198643f2c
techno\alexey.serov <alexey.serov@technomos.ru>
2016-09-06 16:27:36 +0300

    cnt_music_history fix

--- src/Ownradio.DB/tables/tab_db_cnt_music_history.sql
+++ src/Ownradio.DB/tables/tab_db_cnt_music_history.sql
@@ -1,12 +1,13 @@
 CREATE TABLE db.cnt_music_history( 
+ id SERIAL,
  user_id VARCHAR(128) NOT NULL,
  music_id INTEGER NOT NULL,
  description varchar(1024),
  changed_in varchar(128),
  lastupdatedatetime TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
-PRIMARY KEY(user_id, music_id)) WITH (oids = true);
+PRIMARY KEY(id));
 
-COMMENT ON TABLE db.cnt_music_history IS 'Music ratio';
+COMMENT ON TABLE db.cnt_music_history IS 'Music changes history';
 COMMENT ON COLUMN db.cnt_music_history.user_id IS 'Identifies the user';
 COMMENT ON COLUMN db.cnt_music_history.music_id IS 'Identifies the music';
 COMMENT ON COLUMN db.cnt_music_history.description IS 'Changes description';
