50a52884bd346bc7bcc99455bc98cd3c61b3888e
panchenkodp <panchenkodp@gmail.com>
2016-09-17 15:56:39 +0300

    Добавил модуль документирования методов. Добавил контроллер работы с треками

---
+++ src/OwnRadio.Server.Core/OwnRadio.Web.Api/src/OwnRadio.Web.Api/Controllers/TrackController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OwnRadio.Web.Api.Controllers
+{
+	[Route("api/[controller]")]
+	public class TrackController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		// GET api/track/5
+		[HttpGet("{id}")]
+		public string GetNextTrackID(int id)
+		{
+			return "value";
+		}
+
+	}
+}
