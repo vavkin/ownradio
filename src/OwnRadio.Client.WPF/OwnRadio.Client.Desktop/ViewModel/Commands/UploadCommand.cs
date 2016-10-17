2db32473bb203cea8bee8b58cd14c012f66e9f70
Alex <danilov.cpp@gmail.com>
2016-10-17 13:04:36 +0600

    Заготовка Upload
    Добавил кнопку Upload и заготовку Upload-handler'а

---
+++ src/OwnRadio.Client.WPF/OwnRadio.Client.Desktop/ViewModel/Commands/UploadCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Forms;
+
+namespace OwnRadio.Client.Desktop.ViewModel.Commands
+{
+    public class UploadCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var dialog = new FolderBrowserDialog();
+            if (dialog.ShowDialog() != DialogResult.OK)
+                return;
+
+            var dirs = Directory.GetFiles(dialog.SelectedPath, "*.mp3");
+
+            MessageBox.Show("Selected: " + dirs.Length);
+
+            foreach (var dir in dirs)
+            {
+                // Upload
+            }
+        }
+    }
+}
---
+++ src/OwnRadio.Client.WPF/OwnRadio.Client.Desktop/ViewModel/ViewModelUploader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using OwnRadio.Client.Desktop.ViewModel.Commands;
+
+namespace OwnRadio.Client.Desktop.ViewModel
+{
+    public class ViewModelUploader : DependencyObject
+    {
+        public UploadCommand UploadCommand { get; set; }
+        
+        public int Count
+        {
+            get { return (int)GetValue(CountProperty); }
+            set { SetValue(CountProperty, value); }
+        }
+
+        public static readonly DependencyProperty CountProperty =
+            DependencyProperty.Register("Count", typeof(int), typeof(ViewModelUploader), new PropertyMetadata(0));
+        
+        public ViewModelUploader()
+        {
+            this.UploadCommand = new UploadCommand();
+        }
+
+        public void Upload()
+        {
+
+        }
+    }
+}
